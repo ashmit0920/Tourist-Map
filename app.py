d4a614604ae32f9a134a7b94407dd353eb0e7d9c
unknown <ashmitthawait2@gmail.com>
2024-07-10 02:24:46 +0530

    testing for paris

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import streamlit as st
+import folium
 from streamlit_folium import st_folium
-from main import display_map
 
-st.set_page_config(page_title="Testing Folium")
+mymap = folium.Map(location=[48.8566, 2.3522], zoom_start=13)  # Centered on Paris, France
 
-st.title("Map")
+tourist_spots = {
+    "Eiffel Tower": [48.8584, 2.2945],
+    "Louvre Museum": [48.8606, 2.3376],
+    "Notre-Dame Cathedral": [48.8529, 2.3500],
+}
 
-st_folium(display_map())
+for spot, coords in tourist_spots.items():
+    folium.Marker(coords, popup=spot).add_to(mymap)
+
+st.title("Tourist Map of Paris")
+st_folium(mymap, width=700, height=500)
